d3be1989c6287462b764b981be3e9f32cfc4d2fa
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2021-01-26 09:14:17 -0500

    still working on server

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import * as child from 'child_process';
 import axios from 'axios';
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 app.get('/test', async (req, res) => {
 	try {
-		console.log("hallo")
+		console.log(req.body)
 		const {
 			bedroom,
 			bathroom,
@@ -51,8 +55,8 @@ app.get('/test', async (req, res) => {
 	} catch (error) {
 		console.error(error)
 	}
-})
+});
 
 
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log('App is up!'));
